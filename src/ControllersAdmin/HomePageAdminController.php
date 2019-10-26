88d57782d198f85b43f7349dfeb01d1f77111522
gbrle <david.gabriele@outlook.fr>
2019-10-26 04:21:52 +0200

    Creation du controller admin

---
+++ src/ControllersAdmin/HomePageAdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\ControllersAdmin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePageAdminController extends AbstractController
+{
+    /**
+     * @Route("admin/home", name="home_page_admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'HomePageAdminController',
+        ]);
+    }
+}
